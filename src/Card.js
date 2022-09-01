5d9dcf18f9a38992709ab662b80f532f59e049bf
Burneikis <87457198+AlexBurneikis@users.noreply.github.com>
2022-09-01 17:08:15 +1000

    selection

--- src/Card.js
+++ src/Card.js
@@ -1,31 +1,33 @@
 import React from "react";
 
-export default class Card extends React.Component {
-  constructor(props) {
-    super(props);
-    this.card = props.card;
-    this.number = props.number;
-    this.state = {
-      clicked: false,
-    };
-    this.handleClick = () => {
-      this.setState({
-        clicked: !this.state.clicked,
-      })
-    }
-  };
+function Card(props) {
+  const [selected, setSelected] = React.useState(false);
 
-  render() {
-    return (
-      <img
-        style={{
-          border: this.state.clicked ? "2px dotted #ccc" : "2px dotted #fff",
-          borderRadius: "10px",
-        }}
-        alt="card"
-        src={`./assets/${this.number}.png`}
-        onClick={this.handleClick}
-      ></img>
-    );
+  let handleSelectedCards = props.function
+  let selectedCards = props.selectedCards
+  let card = props.card;
+  let number = props.number;
+
+  function handleClick() {
+    console.log(selectedCards.length)
+    setSelected(false)
+    handleSelectedCards(card)
+    if (selectedCards.includes(card)) {
+      setSelected(true)
+    }
   }
+
+  return (
+    <img
+      style={{
+        border: selected ? "2px dotted #ccc" : "2px dotted #fff",
+        borderRadius: "10px",
+      }}
+      alt="card"
+      src={`./assets/${number}.png`}
+      onClick={handleClick}
+    ></img>
+  );
 }
+
+export default Card;
--- src/Cards.js
+++ src/Cards.js
@@ -10,8 +10,21 @@ function getCardNumber(card) {
 }
 
 function Cards() {
+  let selectedCards = [];
+
+  function handleSelectedCards(card) {
+    if (selectedCards.includes(card)) {
+      selectedCards.splice(selectedCards.indexOf(card), 1);
+      return
+    }
+    if (selectedCards.length > 2) {
+      return
+    }
+    selectedCards.push(card);
+  }
+
   return playCards.map((card, index) => {
-    return <Card key={index} card={card} number={getCardNumber(card)} />;
+    return <Card selectedCards={selectedCards} function={handleSelectedCards} key={index} card={card} number={getCardNumber(card)} />;
   });
 }
 
